64e14b99e6bd34473c9f55fffd22cb13d4383439
samgrant-unity <sam.grant@unity3d.com>
2023-10-19 11:24:54 +0100

    Added hoop-count to CS and score to LB

--- VRHandler/Project/Progress.cs
+++ VRHandler/Project/Progress.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Unity.Services.CloudCode.Core;
 using Unity.Services.CloudCode.Apis;
+using Unity.Services.CloudSave.Model;
+using Unity.Services.Leaderboards.Model;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Linq;
+using System;
 
 namespace HelloWorld
 {
@@ -22,38 +24,64 @@ namespace HelloWorld
 
     public class VRProgressService : IProgressService
     {
-        PushClient _pushClient;
         IGameApiClient _apiClient;
-        ILogger<VRHandlerModule> _logger;
-        
-        public VRProgressService(ILogger<VRHandlerModule> logger, IGameApiClient apiClient, PushClient pushClient)
+
+        const string dailyHoopCountKey = "daily-hoop-count";
+        const string leaderboardId = "scores";
+
+        public VRProgressService(IGameApiClient apiClient)
         {
-            _pushClient = pushClient;
             _apiClient = apiClient;
-            _logger = logger;
         }
 
         public async Task<int> AddScore(IExecutionContext ctx, ScoreEventData data)
         {
-            var result = _apiClient.RemoteConfigSettings.AssignSettingsGetAsync(ctx, ctx.AccessToken, ctx.ProjectId,
+            var rcResult = _apiClient.RemoteConfigSettings.AssignSettingsGetAsync(ctx, ctx.AccessToken, ctx.ProjectId,
                             ctx.ProjectId, null, new List<string> { "progressXP", "spawnDelay", "hoops" });
 
-            var settings = result.Result.Data.Configs.Settings;
+            var settings = rcResult.Result.Data.Configs.Settings;
 
             var xp = settings["progressXP"];
             var tick = settings["spawnDelay"];
             var hoops = JsonSerializer.Deserialize<Hoop[]>((string)settings["hoops"]);
             int score = 0;
 
+            if (hoops == null || hoops.Length <= 0)
+            {
+                return 0;
+            }
+
             foreach (var item in hoops)
             {
-                if (item.ID == data.HoopId) {
+                if (item.ID == data.HoopId)
+                {
                     score = item.Score;
                     break;
                 }
             }
 
-            return score;
+            var csResult = await _apiClient.CloudSaveData.GetItemsAsync(
+                ctx, ctx.AccessToken, ctx.ProjectId, ctx.PlayerId, new List<string> { dailyHoopCountKey });
+            var csItem = csResult.Data.Results.First();
+            var currentDailyHoopCount = 0;
+
+            if (csItem != null)
+            {
+                var lastModified = csItem.Modified;
+                // Check whether the last value was set today in order to increment
+                if (lastModified.Date?.Date == DateTime.Today)
+                {
+                    currentDailyHoopCount = (int)csItem.Value;
+                }
+            }
+
+            await _apiClient.CloudSaveData.SetItemAsync(
+                ctx, ctx.AccessToken, ctx.ProjectId, ctx.PlayerId, new SetItemBody(dailyHoopCountKey, currentDailyHoopCount + 1));
+
+            var addScoreResult = await _apiClient.Leaderboards.AddLeaderboardPlayerScoreAsync(
+                ctx, ctx.AccessToken, Guid.Parse(ctx.ProjectId), leaderboardId, ctx.PlayerId, new LeaderboardScore(score));
+
+            return (int)addScoreResult.Data.Score;
         }
     }
 }
